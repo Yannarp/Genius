35a4fbe72f0dfff5fa7e1fabf47314dc47daa4b4
Yanna Pecanha <yannarp@hotmail.com>
2022-03-31 14:46:01 -0300

    Upgrade

--- js/script.js
+++ js/script.js
@@ -1,107 +1,172 @@
 let order = [];
 let clickedOrder = [];
 let score = 0;
+let isTiming = false;
 
-//0 - verde
-//1 - vermelho
-//2 - amarelo
-//3 - azul
+// Novas variáveis globais: contadores para player
+// e sequência de cores
+var number = 0;
+var counter = 0;
+var countPlayer = 0;
+var music_back = 0;
 
-const blue = document.querySelector('.blue');
-const red = document.querySelector('.red');
-const green = document.querySelector('.green');
-const yellow = document.querySelector('.yellow');
+// Lista de músicas disponíveis no diretório
+var arrayPlayer = [
+ 	'Christina.Aguilera-feel.this.moment',
+	'Will u0026 Tim   Song Of Storms (Zelda Theme Remix)',
+	'Zelda Techno Remix   Ballad of the Goddess'
+];
 
-// Criar ordem aleatoria de cores
-    let shuffleOrder = () => {
-    let colorOrder = Math.floor(Math.random() * 4);
-    order[order.length] = colorOrder;
-    clickedOrder = [];
+// Array para eventos dos players
+var musicPlayer = [];
 
-    for(let i in order) {
-        let elementColor = createColorElement(order[i]);
-        lightColor(elementColor, Number(i) + 1);
-    }
+// Constantes dos elementos HTML
+const BLUE_DIV   = document.querySelector('.blue');
+const RED_DIV    = document.querySelector('.red');
+const GREEN_DIV  = document.querySelector('.green');
+const YELLOW_DIV = document.querySelector('.yellow');
+
+// Constante do H3 para nome da música
+const nameMusic = document.getElementById('name-music');
+
+
+// Esta função cria ordem aleatória de cores
+let shuffleOrder = () => {
+	let colorOrder = Math.floor(Math.random() * 4);
+	order[order.length] = colorOrder;
+	clickedOrder = [];
+
+	for(let i in order){
+		let elementOrder = createColorElement(order[i]);
+		let classElement = getClassElement(order[i]);
+		lightColor(elementOrder, classElement);
+	}
+
 }
 
-//acende a proxima cor
-let lightColor = (element, number) => {
-    number = number * 500;
-    setTimeout(() => {
-        element.classList.add('selected');
-    }, number - 250);
-    setTimeout(() => {
-        element.classList.remove('selected');
-    });
+// Torna mais clara a próxima cor
+let lightColor = (element, classElement) => {
+	number = number + 250;
+
+	// Aqui teve um bug e foi corrigido, cada cor deve piscar por vez!
+	setTimeout(() => {
+			element.classList.add(classElement);
+	}, number);
+
+	number = number + 250;
+
+	setTimeout(() => {
+		element.classList.remove(classElement);
+		counter++;
+		if(counter === order.length){
+			counter = 0;
+			number = 0;
+		}
+	}, number);
+
 }
 
-//checa se os botoes clicados são os mesmos gerados no jogo
+// Identifica se os botões clicados é a mesma ordem do jogo
 let checkOrder = () => {
-    for(let i in clickedOrder) {
-        if(clickedOrder[i] != order[i]) {
-            gameOver();
-            break;
-        }
-    }
-    if(clickedOrder.length == order.length) {
-        alert(`Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!`);
-        nextLevel();
-    }
+	for(let i in clickedOrder){
+		if(clickedOrder[i] != order[i]){
+			gameover();
+			break;
+		}
+	}
+
+	if(clickedOrder.length == order.length){
+		alert(`Seus pontos são ${score}\nVocê acertou! Iniciando próximo nível`);
+		nextLevel();
+	}
 }
-//função para o click do usario
+
+// Função para cliques dos usuários
 let click = (color) => {
-    clickedOrder[clickedOrder.length] = color;
-    createColorElement(color).classList.add('selected');
+	clickedOrder[clickedOrder.length] = color;
+	createColorElement(color).classList.add(getClassElement(color));
 
-    setTimeout(() => {
-        createColorElement(color).classList.remove('selected');
-        checkOrder();
-    },250);
+	setTimeout(() => {
+		createColorElement(color).classList.remove(getClassElement(color));
+		checkOrder();
+	}, 250);
 }
 
-//funcaoo que retorna a cor 
+// Função que retorna a cor
 let createColorElement = (color) => {
-    if(color == 0) {
-        return green;
-    } else if(color == 1) {
-        return red;
-    } else if (color == 2) {
-        return yellow;
-    } else if (color == 3) {
-        return blue;
-    }
+	switch(color){
+		case 0: return GREEN_DIV;
+		case 1: return RED_DIV;
+		case 2: return YELLOW_DIV;
+		case 3: return BLUE_DIV;
+	}
 }
 
-//funcaoo para proximo nivel do jogo
+let getClassElement = (color) => {
+	switch(color){
+		case 0: return 'selected-green';
+		case 1: return 'selected-red';
+		case 2: return 'selected-yellow';
+		case 3: return 'selected-blue';
+	}
+}
+
+// Função que vai para o próximo nível
 let nextLevel = () => {
-    score++;
-    shuffleOrder();
-}
-    
-//funcao para game over
-let gameOver = () => {
-    alert(`Pontuação: ${score}!\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo`);
-    order = [];
-    clickedOrder = [];
-
-    playGame();
+	score++;
+	shuffleOrder();
 }
 
-//funcao de inicio do jogo
+// Função para final de jogo caso perdeu
+let gameover = () => {
+	alert(`Seus pontos são ${score}\nVocê perdeu! :(\n Reinicie o jogo em Ok`);
+	order = [];
+	clickedOrder = [];
+
+	playGame();
+}
+
+// Função para iniciar o jogo
 let playGame = () => {
-    alert('Bem vindo ao Gênesis! Iniciando novo jogo!');
-    score = 0;
 
-    nextLevel();
+	// Inicia música específica no Array e exibe o nome
+	setInterval(() => {
+	 	musicPlayer[countPlayer].play();
+	 	nameMusic.innerHTML = arrayPlayer[countPlayer]
+	 				.replace('-', ' - ')
+	 				.replaceAll('.', ' ') + ' reproduzindo...';
+	}, 100);
+
+	alert("Let´s go The Genius! Vamos dançar e memorizar?");
+	score = 0;
+
+	nextLevel();
 }
 
-//eventos de clique para as cores 
-green.onclick = () => click(0);
-red.onclick = () => click(1);
-yellow.onclick = () => click(2);
-blue.onclick = () => click(3);
+// Função para adicionar músicas na lista e evento de terminado
+// É tocado 3 músicas repetidamente
+let initMusics = () => {
 
-//inicio do jogo
+	for(let i = 0; i < arrayPlayer.length; i++){
+		musicPlayer.push(document.getElementById(arrayPlayer[i]));
+		
+		musicPlayer[i].addEventListener('ended', function(){
+	 		musicPlayer[i].currentTime = 0;
+	 		musicPlayer[i].pause();
+	 		countPlayer++;
+	 		
+	 		if(countPlayer === arrayPlayer.length) countPlayer = 0;
+	 	});
+	}
+
+}
+
+// Eventos dos botões para as cores
+GREEN_DIV.onclick = () => click(0);
+RED_DIV.onclick = () => click(1);
+YELLOW_DIV.onclick = () => click(2);
+BLUE_DIV.onclick = () => click(3);
+
+// Chama a função para adicionar músicas e iniciar jogo
+initMusics();
 playGame();
-
-
